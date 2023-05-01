64c2ce7d6b3e1cbbec0f5b23705142427b1d5c2c
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-05-01 18:57:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,72 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T val;
+        Node* next;
+    };
+    Node* header;
+public:
+    TPQueue() : header(nullptr) {}
+    T pop();
+    void push(const T& res);
 };
+template<typename T>
+void TPQueue<T>::push(const T& res) {
+    if (header == NULL) {
+        Node* str = new Node;
+        str->val = res;
+        str->next = NULL;
+        header = str;
+    }
+    else {
+        Node* c1 = header;
+        Node* c2 = new Node;
+        int n = 0;
+        while (c1) {
+            if (c1->val.prior < res.prior) {
+                break;
+            }
+            else {
+                c1 = c1->next;
+                n++;
+            }
+        }
+        if (n == 0) {
+            c2->next = header;
+            c2->val = res;
+            header = c2;
+        }
+        else {
+            c1 = header;
+            for (int i = 1; i < n; i++) {
+                c1 = c1->next;
+            }
+            c2->next = c1->next;
+            c2->val = res;
+            c1->next = c2;
+        }
+    }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+    if (header == NULL) {
+        throw std::string("!!!");
+    }
+    else {
+        T res = header->val;
+        Node* c = header->next;
+        delete header;
+        header = c;
+        return res;
+    }
+}
+
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
